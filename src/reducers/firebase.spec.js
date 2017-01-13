ebb4b334ae1604875895703c7916fa3d84daa65e
jhar <jhar87@gmail.com>
2017-01-13 06:29:52 -0800

    Early tests for firebase reducer

--- src/reducers/firebase.spec.js
+++ src/reducers/firebase.spec.js
@@ -1,40 +1,72 @@
 import deepFreeze from 'deep-freeze'
 import firebase from './firebase'
-import {
-  GOOGLE_SIGN_IN,
-  GOOGLE_SIGN_OUT
-} from '../constants/actionTypes'
 
-const potentiallyMutableState = {
+const maybeMutableState = {
   firebase: {
     token: 'adsfjasdklfjef',
     user: {
       name: 'steve',
       email: 'baloney@sandwich.com'
     }
-  },
-  three: {
-    something: {
-      is: true,
-      not: false
-    }
-  },
-  ui: {
-    dashboard: {
-      animate: true,
-      open: true
-    }
   }
 }
 
-deepFreeze(potentiallyMutableState)
+const GOOGLE_SIGN_IN = {
+  token: 'askdjfalskdjf',
+  type: 'GOOGLE_SIGN_IN',
+  user: 'justin'
+}
+
+const GOOGLE_SIGN_OUT = {
+  type: 'GOOGLE_SIGN_OUT'
+}
+
+deepFreeze(maybeMutableState)
 deepFreeze(GOOGLE_SIGN_IN)
 deepFreeze(GOOGLE_SIGN_OUT)
 
-const passingTest = true
+describe('Deep Freeze', () => {
+  it('will not let me mutate a frozen object', () => {
+    function hamSandwich(obj) {
+      obj.firebase.user.name = 'ham sandwich'
+    }
+    expect(() => {
+      hamSandwich(maybeMutableState)
+    }).toThrow()
+  })
+})
 
 describe('The firebase reducer', () => {
   it('works with Jest', () => {
+    const passingTest = true
     expect(passingTest).toBeTruthy()
+  })
+
+  it('can recognize the same state object', () => {
+    expect(maybeMutableState).toBe(maybeMutableState)
+  })
+
+  it('does not mutate state on sign in', () => {
+    expect(() => {
+      firebase(maybeMutableState, GOOGLE_SIGN_IN)
+    }).not.toBe(maybeMutableState)
+  })
+
+  it('does not mutate state on sign out', () => {
+    expect(() => {
+      firebase(maybeMutableState, GOOGLE_SIGN_OUT)
+    }).not.toBe(maybeMutableState)
+  })
+
+  const newState = firebase(maybeMutableState, GOOGLE_SIGN_OUT)
+
+  it('will leave the user undefined after signing out', () => {
+    expect(newState.user).toBeUndefined()
+  })
+
+  const newNewState = firebase(newState, GOOGLE_SIGN_IN)
+
+  it('can immediately sign me back in with correct values', () => {
+    expect(newNewState.user).toEqual('justin')
   })
 })
